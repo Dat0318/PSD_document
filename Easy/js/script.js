937e72b56f49b94229d0bc7645cf0f67d708a76e
Tran Duy Dat <dattd@apecsoft.asia>
2021-01-06 11:41:41 +0700

    add easy and mogo reponsive

--- Easy/js/script.js
+++ Easy/js/script.js
@@ -5,3 +5,8 @@ var swiper = new Swiper('.swiper-container', {
   },
 });
 // END: Slider
+$(document).ready(function () {
+  $('.menu-bar').click(function () {
+    $('.menu').fadeToggle();
+  });
+});
